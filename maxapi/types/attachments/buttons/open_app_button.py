641681fc64dbcc4821495216140fbc0f4e987411
Denis <bestloveapples@gmail.com>
2025-12-14 23:36:40 +0300

    Добавлен атрибут payload

--- maxapi/types/attachments/buttons/open_app_button.py
+++ maxapi/types/attachments/buttons/open_app_button.py
@@ -12,9 +12,11 @@ class OpenAppButton(Button):
         text: Видимый текст кнопки
         web_app: Публичное имя (username) бота или ссылка на него, чьё мини-приложение надо запустить
         contact_id: Идентификатор бота, чьё мини-приложение надо запустить
+        payload: Параметр запуска, который будет передан в initData мини-приложения
     """
 
     type: ButtonType = ButtonType.OPEN_APP
     text: str
     web_app: Optional[str] = None
     contact_id: Optional[int] = None
+    payload: Optional[str] = None
