2e62d33249007616333da38c0a64cd4766956a82
4qwerty1 <prosin270901@gmail.com>
2026-02-03 09:43:40 +1200

    refactor: Добавил RedisStorage в storage.__init__

--- maxapi/storage/__init__.py
+++ maxapi/storage/__init__.py
@@ -1,4 +1,5 @@
 from .base import StorageKey, BaseStorage
 from .memory import MemoryStorage
+from .redis import RedisStorage
 
-__all__ = ['StorageKey', 'BaseStorage', 'MemoryStorage']
+__all__ = ['StorageKey', 'BaseStorage', 'MemoryStorage', 'RedisStorage']
