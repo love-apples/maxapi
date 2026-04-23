4d5a7bff5d0ca5c81b9d6e02a10666569efa45ef
Pankovea <114323250+Pankovea@users.noreply.github.com>
2026-04-23 15:04:11 +0300

    feat: утилита генерации ссылок на сообщения * build_message_link * Конвертаци:     mid_to_chatid_seq     chatid_seq_to_mid
    add: Валидация функций
    add: link_to_chatid_seq - обратное преобразование
    * Валидация mid перенесена в функцию mid_to_chatid_seq
    add: Тесты на реальных данных проходят успешно.

---
+++ maxapi/utils/__init__.py
@@ -0,0 +1,13 @@
+from .message_link import (
+    mid_to_chatid_seq,
+    chatid_seq_to_mid,
+    build_message_link,
+    link_to_chatid_seq,
+)
+
+__all__ = [
+    "mid_to_chatid_seq",
+    "chatid_seq_to_mid",
+    "build_message_link",
+    "link_to_chatid_seq",
+]
