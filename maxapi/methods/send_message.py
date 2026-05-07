046798bdb62f498ef459d66397a8914a801ff8e5
Demas96 <dmitrijjkizima@yandex.ru>
2026-05-07 15:31:24 +0300

    пропуск пустых вложений клавиатуры (#130)
    * пропуск пустых вложений клавиатуры
    
    * Добавлен тест пропуска пустых вложений клавиатуры
    
    * Уточнена проверка пустой inline-клавиатуры
    
    * Уточнена проверка пустой inline-клавиатуры
    
    * Проверка пустой inline-клавиатуры приведена к AttachmentButton
    
    * Дотипизирован AttachmentButton для mypy
    
    ---------
    
    Co-authored-by: dkizima <dmitry.kizima@iqnergy.com>

--- maxapi/methods/send_message.py
+++ maxapi/methods/send_message.py
@@ -11,6 +11,7 @@ from ..exceptions.max import MaxApiError
 from ..loggers import logger_bot
 from ..types.attachments import Attachments
 from ..types.attachments.attachment import Attachment
+from ..types.attachments.buttons.attachment_button import AttachmentButton
 from ..types.attachments.upload import AttachmentUpload
 from ..types.input_media import InputMedia, InputMediaBuffer
 from ..types.message import NewMessageLink
@@ -134,6 +135,11 @@ class SendMessage(BaseConnection):
 
         if self.attachments:
             for att in self.attachments:
+                if isinstance(att, AttachmentButton) and not any(
+                    att.payload.buttons
+                ):
+                    continue
+
                 if isinstance(att, (InputMedia, InputMediaBuffer)):
                     has_input_media = True
 
